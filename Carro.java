e8158bd9f6508a2519459a048750eb59926e68c6
Guilu3105 <guilhermealves3105@gmail.com>
2023-08-18 16:20:32 -0300

    CARRO

---
+++ Carro.java
@@ -0,0 +1,8 @@
+public class Carro {
+    public static final String VERMELHO = "VERMELHO";
+    public static final String ROXO = "ROXO";
+
+    public void setCor(String cor) {
+        System.out.println(cor);
+    }
+}
